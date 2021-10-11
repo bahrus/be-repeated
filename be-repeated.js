ece09c8f4c2ac2a4901811f44af7666e1fc7b4b5
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-11 14:59:24 -0400

    ...

--- be-repeated.js
+++ be-repeated.js
@@ -7,14 +7,36 @@ const ce = new CE({
             upgrade: '*',
             ifWantsToBe: 'repeated',
             forceVisible: true,
-            virtualProps: ['eventHandlers', 'list', 'transform']
+            virtualProps: ['eventHandlers', 'list', 'transform',]
         }
     },
     complexPropDefaults: {
-        actions: [],
+        actions: [
+            ({ list, transform, self }) => {
+                if (list === undefined || transform === undefined)
+                    return;
+                let tail = self;
+                for (const item of list) {
+                    const clone = self.content.cloneNode(true);
+                    const children = Array.from(clone.children);
+                    for (const child of children) {
+                        tail.insertAdjacentElement('afterend', child);
+                        tail = child;
+                    }
+                }
+            }
+        ],
         on: {},
-        init: (self) => {
-            console.log(self);
+        init: (self, xtalDecor, target) => {
+            if (self.localName !== 'template') {
+                const templ = document.createElement('template');
+                const attrIs = 'is-' + xtalDecor.ifWantsToBe;
+                const attrBe = 'be-' + xtalDecor.ifWantsToBe;
+                templ.setAttribute(attrBe, self.getAttribute(attrIs));
+                self.insertAdjacentElement('afterend', templ);
+                target.removeAttribute(attrIs);
+                templ.content.appendChild(target);
+            }
         },
         finale: (self, target) => {
             if (target.localName !== 'template')
--- be-repeated.ts
+++ be-repeated.ts
@@ -12,15 +12,35 @@ const ce = new CE<XtalDecorCore<Element>>({
             upgrade: '*',
             ifWantsToBe: 'repeated',
             forceVisible: true,
-            virtualProps: ['eventHandlers', 'list', 'transform']
+            virtualProps: ['eventHandlers', 'list', 'transform', ]
         }
     },
     complexPropDefaults: {
         actions:[
+            ({list, transform, self}) => {
+                if(list === undefined || transform === undefined) return;
+                let tail = self;
+                for(const item of list){
+                    const clone = self.content.cloneNode(true);
+                    const children = Array.from(clone.children);
+                    for(const child of children){
+                        tail.insertAdjacentElement('afterend', child)!;
+                        tail = child;
+                    }
+                }
+            }
         ],
         on:{},
-        init:(self: Element) => {
-            console.log(self);
+        init:(self: Element, xtalDecor: XtalDecorProps<Element>, target: Element) => {
+            if(self.localName !== 'template'){
+                const templ = document.createElement('template');
+                const attrIs = 'is-' + xtalDecor.ifWantsToBe;
+                const attrBe = 'be-' + xtalDecor.ifWantsToBe;
+                templ.setAttribute(attrBe, self.getAttribute(attrIs)!);
+                self.insertAdjacentElement('afterend', templ);
+                target.removeAttribute(attrIs);
+                templ.content.appendChild(target);
+            }
         },
         finale:(self: Element, target: Element) => {
             if(target.localName !== 'template') return;
