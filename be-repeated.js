56b912d437cd43d3937e7e8d7a3e4e9f63a0101d
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-15 15:45:38 -0400

    ...

--- be-repeated.js
+++ be-repeated.js
@@ -1,140 +1,150 @@
-import { XtalDecor } from 'xtal-decor/xtal-decor.js';
-import { CE } from 'trans-render/lib/CE.js';
+import { define } from 'be-decorated/be-decorated.js';
+import { getElementToObserve, addListener } from 'be-observant/be-observant.js';
 import { PE } from 'trans-render/lib/PE.js';
 import { SplitText } from 'trans-render/lib/SplitText.js';
-import { getElementToObserve, addListener } from 'be-observant/be-observant.js';
 import { transform as xf, processTargets } from 'trans-render/lib/transform.js';
 const firstElementMap = new WeakMap();
-const ce = new CE({
+export class BeRepeatedController {
+    intro(proxy, target, beDecorProps) {
+        if (proxy.localName !== 'template') {
+            const templ = document.createElement('template');
+            const attrIs = 'is-' + beDecorProps.ifWantsToBe;
+            const attrBe = 'be-' + beDecorProps.ifWantsToBe;
+            templ.setAttribute(attrBe, proxy.getAttribute(attrIs));
+            proxy.insertAdjacentElement('afterend', templ);
+            target.removeAttribute(attrIs);
+            const clonedTarget = target.cloneNode(true);
+            firstElementMap.set(templ, target);
+            const attribs = clonedTarget.attributes;
+            for (const attrib of attribs) {
+                const name = attrib.name;
+                if (name.startsWith('is-')) {
+                    const newName = 'be-' + name.substr(3);
+                    clonedTarget.setAttribute(newName, attrib.value);
+                    clonedTarget.removeAttribute(name);
+                }
+            }
+            templ.content.appendChild(clonedTarget);
+        }
+        else {
+            proxy.templ = target;
+        }
+    }
+    finale(proxy, target) {
+        if (target.localName !== 'template')
+            return; //[TODO]: ?
+    }
+    onList({ list, proxy }) {
+        if (Array.isArray(list)) {
+            proxy.listVal = list;
+            return;
+        }
+        const observeParams = ((typeof list === 'string') ? { vft: list } : list);
+        const elementToObserve = getElementToObserve(proxy, observeParams);
+        if (elementToObserve === null) {
+            console.warn({ msg: '404', observeParams });
+            return;
+        }
+        addListener(elementToObserve, observeParams, 'listVal', proxy);
+    }
+    renderList({ listVal, transform, proxy, templ }) {
+        let ctx = proxy.ctx;
+        let firstTime = false;
+        if (ctx === undefined) {
+            firstTime = true;
+            ctx = {
+                match: transform,
+                postMatch: [
+                    {
+                        rhsType: Array,
+                        rhsHeadType: Object,
+                        ctor: PE
+                    },
+                    {
+                        rhsType: Array,
+                        rhsHeadType: String,
+                        ctor: SplitText
+                    },
+                    {
+                        rhsType: String,
+                        ctor: SplitText,
+                    }
+                ],
+            };
+            proxy.ctx = ctx;
+        }
+        let tail = proxy;
+        let cnt = 0;
+        let idx = 0;
+        for (const item of listVal) {
+            ctx.host = item;
+            if (firstTime) {
+                const rs = cloneAndTransform(idx, tail, cnt, ctx, proxy, templ);
+                tail = rs.tail;
+                cnt = rs.cnt;
+                idx = rs.idx;
+            }
+            else {
+                const grp = findGroup(tail, `[data-idx="${idx}"]`);
+                const len = grp.length;
+                if (len > 0) {
+                    for (const el of grp) {
+                        el.classList.remove('be-repeated-hidden');
+                    }
+                    cnt += len + 1; //count template
+                    processTargets(ctx, grp);
+                    tail = grp.pop();
+                    idx++;
+                }
+                else {
+                    const rs = cloneAndTransform(idx, tail, cnt, ctx, proxy);
+                    tail = rs.tail;
+                    cnt = rs.cnt;
+                    idx = rs.idx;
+                }
+            }
+        }
+        const prevCnt = Number(proxy.dataset.cnt);
+        while (idx < prevCnt) {
+            const grp = findGroup(tail, `[data-idx="${idx}"]`);
+            const len = grp.length;
+            if (len > 0) {
+                for (const el of grp) {
+                    el.classList.add('be-repeated-hidden');
+                }
+                cnt += len + 1;
+            }
+            idx++;
+        }
+        if (cnt === 0)
+            debugger;
+        proxy.dataset.cnt = cnt.toString();
+    }
+}
+const tagName = 'be-repeated';
+define({
     config: {
-        tagName: 'be-repeated',
+        tagName,
         propDefaults: {
             upgrade: '*',
             ifWantsToBe: 'repeated',
             forceVisible: true,
-            virtualProps: ['eventHandlers', 'list', 'listVal', 'transform', 'ctx']
+            intro: 'intro',
+            finale: 'finale',
+            virtualProps: ['ctx', 'eventHandlers', 'list', 'listVal', 'templ', 'transform'],
+        },
+        actions: {
+            onList: {
+                ifAllOf: ['list']
+            },
+            renderList: {
+                ifAllOf: ['transform', 'listVal', 'templ']
+            }
         }
     },
     complexPropDefaults: {
-        actions: [
-            ({ list, self }) => {
-                if (Array.isArray(list)) {
-                    self.listVal = list;
-                    return;
-                }
-                const observeParams = ((typeof list === 'string') ? { vft: list } : list);
-                const elementToObserve = getElementToObserve(self, observeParams);
-                if (elementToObserve === null) {
-                    console.warn({ msg: '404', observeParams });
-                    return;
-                }
-                addListener(elementToObserve, observeParams, 'listVal', self);
-            },
-            ({ listVal, transform, self, target }) => {
-                if (listVal === undefined || transform === undefined)
-                    return;
-                let ctx = self.ctx;
-                let firstTime = false;
-                if (ctx === undefined) {
-                    firstTime = true;
-                    ctx = {
-                        match: transform,
-                        postMatch: [
-                            {
-                                rhsType: Array,
-                                rhsHeadType: Object,
-                                ctor: PE
-                            },
-                            {
-                                rhsType: Array,
-                                rhsHeadType: String,
-                                ctor: SplitText
-                            },
-                            {
-                                rhsType: String,
-                                ctor: SplitText,
-                            }
-                        ],
-                    };
-                    self.ctx = ctx;
-                }
-                let tail = self;
-                let cnt = 0;
-                let idx = 0;
-                for (const item of listVal) {
-                    ctx.host = item;
-                    if (firstTime) {
-                        const rs = cloneAndTransform(idx, tail, cnt, ctx, self, target);
-                        tail = rs.tail;
-                        cnt = rs.cnt;
-                        idx = rs.idx;
-                    }
-                    else {
-                        const grp = findGroup(tail, `[data-idx="${idx}"]`);
-                        const len = grp.length;
-                        if (len > 0) {
-                            for (const el of grp) {
-                                el.classList.remove('be-repeated-hidden');
-                            }
-                            cnt += len + 1; //count template
-                            processTargets(ctx, grp);
-                            tail = grp.pop();
-                            idx++;
-                        }
-                        else {
-                            const rs = cloneAndTransform(idx, tail, cnt, ctx, self);
-                            tail = rs.tail;
-                            cnt = rs.cnt;
-                            idx = rs.idx;
-                        }
-                    }
-                }
-                const prevCnt = Number(self.dataset.cnt);
-                while (idx < prevCnt) {
-                    const grp = findGroup(tail, `[data-idx="${idx}"]`);
-                    const len = grp.length;
-                    if (len > 0) {
-                        for (const el of grp) {
-                            el.classList.add('be-repeated-hidden');
-                        }
-                        cnt += len + 1;
-                    }
-                    idx++;
-                }
-                if (cnt === 0)
-                    debugger;
-                self.dataset.cnt = cnt.toString();
-            }
-        ],
-        on: {},
-        init: (self, xtalDecor, target) => {
-            if (self.localName !== 'template') {
-                const templ = document.createElement('template');
-                const attrIs = 'is-' + xtalDecor.ifWantsToBe;
-                const attrBe = 'be-' + xtalDecor.ifWantsToBe;
-                templ.setAttribute(attrBe, self.getAttribute(attrIs));
-                self.insertAdjacentElement('afterend', templ);
-                target.removeAttribute(attrIs);
-                const clonedTarget = target.cloneNode(true);
-                firstElementMap.set(templ, target);
-                const attribs = clonedTarget.attributes;
-                for (const attrib of attribs) {
-                    const name = attrib.name;
-                    if (name.startsWith('is-')) {
-                        const newName = 'be-' + name.substr(3);
-                        clonedTarget.setAttribute(newName, attrib.value);
-                        clonedTarget.removeAttribute(name);
-                    }
-                }
-                templ.content.appendChild(clonedTarget);
-            }
-        },
-        finale: (self, target) => {
-            if (target.localName !== 'template')
-                return;
-        }
-    },
-    superclass: XtalDecor
+        controller: BeRepeatedController
+    }
 });
 function findGroup(tail, sel) {
     const returnArr = [];
@@ -191,4 +201,4 @@ function cloneAndTransform(idx, tail, cnt, ctx, self, target) {
     templ.dataset.cnt = templCount.toString();
     return { idx, tail, cnt };
 }
-document.head.appendChild(document.createElement('be-repeated'));
+document.head.appendChild(document.createElement(tagName));
--- be-repeated.ts
+++ be-repeated.ts
@@ -1,146 +1,161 @@
-import {XtalDecor, XtalDecorCore} from 'xtal-decor/xtal-decor.js';
-import { XtalDecorProps } from 'xtal-decor/types';
-import {CE} from 'trans-render/lib/CE.js';
+import {BeRepeatedProps, BeRepeatedActions, BeRepeatedVirtualProps} from './types';
+import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {IObserve} from 'be-observant/types';
+import {getElementToObserve, addListener} from 'be-observant/be-observant.js';
 import { PE } from 'trans-render/lib/PE.js';
 import { SplitText } from 'trans-render/lib/SplitText.js';
-import { IObserve } from 'be-observant/types';
-import {getElementToObserve, addListener} from 'be-observant/be-observant.js';
-import {upShadowSearch} from 'trans-render/lib/upShadowSearch.js';
 import {transform as xf, processTargets} from 'trans-render/lib/transform.js';
 
 const firstElementMap = new WeakMap<HTMLTemplateElement, Element>();
 
-const ce = new CE<XtalDecorCore<Element>>({
+export class BeRepeatedController implements BeRepeatedActions {
+    intro(proxy: Element & BeRepeatedVirtualProps, target: Element, beDecorProps: BeDecoratedProps){
+        if(proxy.localName !== 'template'){
+            const templ = document.createElement('template');
+            const attrIs = 'is-' + beDecorProps.ifWantsToBe;
+            const attrBe = 'be-' + beDecorProps.ifWantsToBe;
+            templ.setAttribute(attrBe, proxy.getAttribute(attrIs)!);
+            proxy.insertAdjacentElement('afterend', templ);
+            target.removeAttribute(attrIs);
+            const clonedTarget = target.cloneNode(true) as Element;
+            firstElementMap.set(templ, target);
+            const attribs = clonedTarget.attributes;
+            for(const attrib of attribs){
+                const name = attrib.name;
+                if(name.startsWith('is-')){
+                    const newName = 'be-' + name.substr(3);
+                    clonedTarget.setAttribute(newName, attrib.value);
+                    clonedTarget.removeAttribute(name);
+                }
+            }
+            templ.content.appendChild(clonedTarget);
+            
+        }else{
+            proxy.templ = target as HTMLTemplateElement;
+        }
+    }
+    finale(proxy: Element & BeRepeatedVirtualProps, target:Element){
+        if(target.localName !== 'template') return; //[TODO]: ?
+    }
+    onList({list, proxy}: this){
+        if(Array.isArray(list)){
+            proxy.listVal = list;
+            return;
+        }
+        const observeParams = ((typeof list === 'string') ? {vft: list} : list) as IObserve;
+        const elementToObserve = getElementToObserve(proxy, observeParams);
+        if(elementToObserve === null){
+            console.warn({msg:'404',observeParams});
+            return;
+        }
+        addListener(elementToObserve, observeParams, 'listVal', proxy);
+    }
+    renderList({listVal, transform, proxy, templ}: this){
+        let ctx = proxy.ctx;
+        let firstTime = false;
+        if(ctx === undefined){
+            firstTime = true;
+            ctx ={
+                match: transform,
+                postMatch: [
+                    {
+                        rhsType: Array,
+                        rhsHeadType: Object,
+                        ctor: PE
+                    },
+                    {
+                        rhsType: Array,
+                        rhsHeadType: String,
+                        ctor: SplitText
+                    },
+                    {
+                        rhsType: String,
+                        ctor: SplitText,
+                    }
+                ],
+            };
+            proxy.ctx = ctx;
+        }
+        
+        let tail = proxy as Element;
+        let cnt = 0;
+        let idx = 0;
+        for(const item of listVal){
+            ctx.host = item;
+            if(firstTime){
+                const rs = cloneAndTransform(idx, tail, cnt, ctx, proxy, templ);
+                tail = rs.tail;
+                cnt = rs.cnt;
+                idx = rs.idx;
+            }else{
+                const grp = findGroup(tail, `[data-idx="${idx}"]`);
+                const len = grp.length;
+                if(len > 0){
+                    for(const el of grp){
+                        el.classList.remove('be-repeated-hidden');
+                    }
+                    cnt += len + 1; //count template
+                    processTargets(ctx, grp);
+                    tail = grp.pop() as Element;
+                    idx++;
+                }else{
+                    const rs = cloneAndTransform(idx, tail, cnt, ctx, proxy);
+                    tail = rs.tail;
+                    cnt = rs.cnt;
+                    idx = rs.idx;         
+                }
+
+            }
+            
+
+        }
+        const prevCnt = Number(proxy.dataset.cnt);
+        while(idx < prevCnt){
+            const grp = findGroup(tail, `[data-idx="${idx}"]`);
+            const len = grp.length;
+            if(len > 0){
+                for(const el of grp){
+                    el.classList.add('be-repeated-hidden');
+                }
+                cnt += len + 1;
+            }
+            idx++;
+            
+        }
+        if(cnt === 0) debugger;
+        proxy.dataset.cnt = cnt.toString();
+    }
+}
+
+export interface BeRepeatedController extends BeRepeatedProps{}
+
+const tagName = 'be-repeated';
+
+define<BeRepeatedProps & BeDecoratedProps<BeRepeatedProps, BeRepeatedActions>, BeRepeatedActions>({
     config:{
-        tagName: 'be-repeated',
+        tagName,
         propDefaults:{
             upgrade: '*',
             ifWantsToBe: 'repeated',
             forceVisible: true,
-            virtualProps: ['eventHandlers', 'list', 'listVal', 'transform', 'ctx']
-        }
-    },
-    complexPropDefaults: {
-        actions:[
-            ({list, self}) => {
-                if(Array.isArray(list)){
-                    self.listVal = list;
-                    return;
-                }
-                const observeParams = ((typeof list === 'string') ? {vft: list} : list) as IObserve;
-                const elementToObserve = getElementToObserve(self, observeParams);
-                if(elementToObserve === null){
-                    console.warn({msg:'404',observeParams});
-                    return;
-                }
-                addListener(elementToObserve, observeParams, 'listVal', self);
-            },
-            ({listVal, transform, self, target}) => {
-                if(listVal === undefined || transform === undefined) return;
-                let ctx = self.ctx;
-                let firstTime = false;
-                if(ctx === undefined){
-                    firstTime = true;
-                    ctx ={
-                        match: transform,
-                        postMatch: [
-                            {
-                                rhsType: Array,
-                                rhsHeadType: Object,
-                                ctor: PE
-                            },
-                            {
-                                rhsType: Array,
-                                rhsHeadType: String,
-                                ctor: SplitText
-                            },
-                            {
-                                rhsType: String,
-                                ctor: SplitText,
-                            }
-                        ],
-                    };
-                    self.ctx = ctx;
-                }
-                
-                let tail = self;
-                let cnt = 0;
-                let idx = 0;
-                for(const item of listVal){
-                    ctx.host = item;
-                    if(firstTime){
-                        const rs = cloneAndTransform(idx, tail, cnt, ctx, self, target);
-                        tail = rs.tail;
-                        cnt = rs.cnt;
-                        idx = rs.idx;
-                    }else{
-                        const grp = findGroup(tail, `[data-idx="${idx}"]`);
-                        const len = grp.length;
-                        if(len > 0){
-                            for(const el of grp){
-                                el.classList.remove('be-repeated-hidden');
-                            }
-                            cnt += len + 1; //count template
-                            processTargets(ctx, grp);
-                            tail = grp.pop();
-                            idx++;
-                        }else{
-                            const rs = cloneAndTransform(idx, tail, cnt, ctx, self);
-                            tail = rs.tail;
-                            cnt = rs.cnt;
-                            idx = rs.idx;         
-                        }
-
-                    }
-                    
-
-                }
-                const prevCnt = Number(self.dataset.cnt);
-                while(idx < prevCnt){
-                    const grp = findGroup(tail, `[data-idx="${idx}"]`);
-                    const len = grp.length;
-                    if(len > 0){
-                        for(const el of grp){
-                            el.classList.add('be-repeated-hidden');
-                        }
-                        cnt += len + 1;
-                    }
-                    idx++;
-                    
-                }
-                if(cnt === 0) debugger;
-                self.dataset.cnt = cnt.toString();
-            }
-        ],
-        on:{},
-        init:(self: Element, xtalDecor: XtalDecorProps<Element>, target: Element) => {
-            if(self.localName !== 'template'){
-                const templ = document.createElement('template');
-                const attrIs = 'is-' + xtalDecor.ifWantsToBe;
-                const attrBe = 'be-' + xtalDecor.ifWantsToBe;
-                templ.setAttribute(attrBe, self.getAttribute(attrIs)!);
-                self.insertAdjacentElement('afterend', templ);
-                target.removeAttribute(attrIs);
-                const clonedTarget = target.cloneNode(true) as Element;
-                firstElementMap.set(templ, target);
-                const attribs = clonedTarget.attributes;
-                for(const attrib of attribs){
-                    const name = attrib.name;
-                    if(name.startsWith('is-')){
-                        const newName = 'be-' + name.substr(3);
-                        clonedTarget.setAttribute(newName, attrib.value);
-                        clonedTarget.removeAttribute(name);
-                    }
-                }
-                templ.content.appendChild(clonedTarget);
-            }
+            intro: 'intro',
+            finale: 'finale',
+            virtualProps: ['ctx', 'eventHandlers', 'list', 'listVal', 'templ', 'transform'],
         },
-        finale:(self: Element, target: Element) => {
-            if(target.localName !== 'template') return;
+        actions:{
+            onList:{
+                ifAllOf:['list']
+            },
+            renderList:{
+                ifAllOf:['transform', 'listVal', 'templ']
+            }
         }
     },
-    superclass: XtalDecor
+    complexPropDefaults:{
+        controller: BeRepeatedController
+    }
 });
+
 function findGroup(tail: Element, sel: string){
     const returnArr: Element[] = [];
     let ns = tail.nextElementSibling;
@@ -161,6 +176,7 @@ function findGroup(tail: Element, sel: string){
     }
     return returnArr;
 }
+
 function cloneAndTransform(idx: number, tail: Element, cnt: number, ctx: any, self: HTMLTemplateElement, target?: HTMLTemplateElement){
     const templ = document.createElement('template');
     templ.dataset.idx = idx.toString();
@@ -190,10 +206,7 @@ function cloneAndTransform(idx: number, tail: Element, cnt: number, ctx: any, se
             tail = child;
         }
     }
-
-
-
     templ.dataset.cnt = templCount.toString();
     return {idx, tail, cnt};
 }
-document.head.appendChild(document.createElement('be-repeated'));
+document.head.appendChild(document.createElement(tagName));
