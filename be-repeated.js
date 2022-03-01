ddb7bf5d36d5eaa839e9a0dc8fcb48ecd0564c3b
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-03-01 06:06:10 -0500

    0.0.53

--- be-repeated.js
+++ be-repeated.js
@@ -50,10 +50,11 @@ export class BeRepeatedController {
     async onList() {
         //TODO:  put back list, proxy in the signature.
         //for now, causes a weird browser dev tools crash when debugging xtal-vlist/demo/dev.html
-        console.log('about to execute code dev tools might crash on.');
+        //Not crashing on edge, only chrome
+        //console.log('about to execute code dev tools might crash on.');
         const list = this.list;
         const proxy = this.proxy;
-        console.log('i survived');
+        //console.log('i survived');
         if (Array.isArray(list)) {
             proxy.listVal = list;
             return;
