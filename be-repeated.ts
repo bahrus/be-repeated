42ff45b6dcd1249d01ad695a56c4dfbe2c2aa067
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-05-26 16:40:18 -0400

    0.0.125

--- be-repeated.ts
+++ be-repeated.ts
@@ -1,3 +1,9 @@
+import {config as beCnfg} from 'be-enhanced/config.js';
+import {BE, BEConfig} from 'be-enhanced/BE.js';
+import {Actions, AllProps, AP, PAP} from './types';
+import { Positractions, PropInfo } from 'trans-render/froop/types';
+import {IEnhancement,  BEAllProps} from 'trans-render/be/types';
+
 export class BeRepeated{
-    
+
 }
